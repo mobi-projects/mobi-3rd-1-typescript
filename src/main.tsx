79e82d9758a2e4086114f0ef4faaf73ba4be93ac
yunshin <2younsin@gmail.com>
2024-05-01 20:26:22 +0900

    depend : tanstack-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,13 @@
+import { QueryClientProvider } from "@tanstack/react-query"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
+import { defaultQueryClient } from "./libs/react-query"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={defaultQueryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
