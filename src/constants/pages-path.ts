16241b72a852e22d092a7e9d545d80f8222310f1
yunshin <2younsin@gmail.com>
2024-05-01 21:18:40 +0900

    feat : 페이지 라우팅

---
+++ src/constants/pages-path.ts
@@ -0,0 +1,6 @@
+export const PATH_HOME = "/" as const
+export const PATH_DETAIL = "/detail-page" as const
+export const PATH_SIGN_IN = "/sign-in-page" as const
+export const PATH_SIGN_UP = "/sign-up-page" as const
+export const PATH_MY = "/my-page" as const
+export const PATH_NOT_FOUND = "/*" as const
--- src/libs/react-router-dom/router.tsx
+++ src/libs/react-router-dom/router.tsx
@@ -1,3 +1,38 @@
+import {
+  PATH_DETAIL,
+  PATH_HOME,
+  PATH_MY,
+  PATH_NOT_FOUND,
+  PATH_SIGN_IN,
+  PATH_SIGN_UP,
+} from "@/constants"
+import { Detail, Home, My, NotFound, SignIn, SignUp } from "@/pages"
+
 import { createBrowserRouter } from "react-router-dom"
 
-export const Router = createBrowserRouter([])
+export const Router = createBrowserRouter([
+  {
+    path: PATH_HOME,
+    element: <Home />,
+  },
+  {
+    path: PATH_DETAIL,
+    element: <Detail />,
+  },
+  {
+    path: PATH_SIGN_IN,
+    element: <SignIn />,
+  },
+  {
+    path: PATH_SIGN_UP,
+    element: <SignUp />,
+  },
+  {
+    path: PATH_MY,
+    element: <My />,
+  },
+  {
+    path: PATH_NOT_FOUND,
+    element: <NotFound />,
+  },
+])
