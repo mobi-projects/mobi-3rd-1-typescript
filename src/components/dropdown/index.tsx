1df39ce56dc83803b3bf85ea4ca361c47aa2cb98
yunshin <2younsin@gmail.com>
2024-05-01 20:33:32 +0900

    comment : 공용컴포넌트 의 (부품/자식) 컴포넌트 관리에 대한 주석 작성

--- src/components/dropdown/index.tsx
+++ src/components/dropdown/index.tsx
@@ -1,3 +1,5 @@
-export const DropDown = () => {
+export const Dropdown = () => {
   return <div>DropDown</div>
 }
+
+/** `Dropdown` 에서 분리시킬 (부품) 컴포넌트가 있다면, export 없이 이 아래(👇) 작성해주세요.! */
--- src/components/pagination/index.tsx
+++ src/components/pagination/index.tsx
@@ -1,3 +1,5 @@
 export const Pagination = () => {
   return <div>Pagination</div>
 }
+
+/** `Pagination` 에서 분리시킬 (부품) 컴포넌트가 있다면, export 없이 이 아래(👇) 작성해주세요.! */
