bb910fb341f0df9d9db7202b5edf0b0247f9ed5e
snkchan <144839872+snkchan@users.noreply.github.com>
2024-05-02 20:05:34 +0900

    chore : 작업전달 사항 docs : notice이용해서 수정

--- src/components/dropdown/dropdown.types.ts
+++ src/components/dropdown/dropdown.types.ts
@@ -1,5 +1,7 @@
-type Option = "perPage" | string // sort에사용될 키값들을 추가해주세요 sort옵션이 다정해지고나면 string은삭제 합니다.
-
+/**
+ * @notice  sort에사용될 키값들을 추가해주세요 sort옵션이 다정해지고나면 string은삭제 합니다.
+ */
+type Option = "perPage" | string //
 export type DropdownProps = {
   items: Array<string>
   option: Option
