e10107a1a3b7cc85225316b37b423d08ecd9daf0
yunshin <2younsin@gmail.com>
2024-05-02 23:37:42 +0900

    feat : dialog 최소 기능 구현

---
+++ src/components/dialog/dialog.type.ts
@@ -0,0 +1,20 @@
+import type { PropsWithChildren } from "react"
+
+export type DialogPT = PropsWithChildren & {
+  onCancel: VoidFunction
+  onConfirm: VoidFunction
+  isAlert?: boolean
+}
+
+export type BackgroundPT = PropsWithChildren & Pick<DialogPT, "onCancel">
+
+export type DialogContextType = {
+  onOpenDialog: onOpenDialogFT
+  onCloseDialog: VoidFunction
+}
+
+export type onOpenDialogFT = (input: DialogPT) => void
+
+export type onAlertFT = (
+  input: PropsWithChildren & Pick<DialogPT, "onConfirm">,
+) => void
--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,17 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
 import React from "react"
 import ReactDOM from "react-dom/client"
 import { RouterProvider } from "react-router-dom"
+import { DialogProvider } from "./components/dialog/dialog.context"
 import { defaultQueryClient } from "./libs/react-query"
 import { Router } from "./libs/react-router-dom"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={defaultQueryClient}>
-      <RouterProvider router={Router} />
-      <ReactQueryDevtools initialIsOpen={true} />
+      <DialogProvider>
+        <RouterProvider router={Router} />
+        <ReactQueryDevtools initialIsOpen={true} />
+      </DialogProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 )
