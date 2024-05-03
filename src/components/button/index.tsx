b796bf1ad3ea4aa88f887a550117193ddbafabcd
snkchan <144839872+snkchan@users.noreply.github.com>
2024-05-03 19:59:16 +0900

    feat : pagination에 맞는 임시 button컴포넌트 생성

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,5 +1,37 @@
-export const Button = () => {
-  return <div>Button</div>
-}
+import React from "react"
+import { cva, type VariantProps } from "class-variance-authority"
 
-/** `Button` 에서 분리시킬 (부품) 컴포넌트가 있다면, export 없이 이 아래(👇) 작성해주세요.! */
+const buttonVariants = cva("button", {
+  variants: {
+    intent: {
+      primary: [
+        "bg-blue-500",
+        "text-white",
+        "border-transparent",
+        "hover:bg-blue-600",
+      ],
+      secondary: [
+        "bg-white",
+        "text-gray-800",
+        "border-gray-400",
+        "hover:bg-gray-100",
+      ],
+    },
+    size: {
+      small: ["text-sm", "py-1", "px-2"],
+      medium: ["text-base", "py-2", "px-4"],
+    },
+  },
+})
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof buttonVariants>
+
+export const Button: React.FC<ButtonProps> = ({
+  className,
+  intent,
+  size,
+  ...props
+}) => (
+  <button className={buttonVariants({ intent, size, className })} {...props} />
+)
