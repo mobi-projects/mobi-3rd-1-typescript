fba6af2e960f2508709f8e65043fb20f49422787
snkchan <144839872+snkchan@users.noreply.github.com>
2024-05-04 03:04:25 +0900

    chorea : 인증관련 경로 상수화

---
+++ src/constants/auth-key.ts
@@ -0,0 +1,4 @@
+export const AUTH_TOKEN = "token" as const
+export const AUTH_KEY = "mobi3rd1234" as const
+export const AUTH_PAIR_NUM = 1 as const
+export const API_SIGNIN = "/user/sign-in" as const
