891e9a75a6c56e01b70de3b7316fdb085300e4de
snkchan <144839872+snkchan@users.noreply.github.com>
2024-05-04 18:45:08 +0900

    chore : refresh요청 signout요청경로 상수로 관리

--- src/constants/auth-key.ts
+++ src/constants/auth-key.ts
@@ -1,4 +1,6 @@
 export const AUTH_TOKEN = "token" as const
 export const AUTH_KEY = "mobi3rd1234" as const
-export const AUTH_PAIR_NUM = 1 as const
+export const AUTH_PAIR_NUM = "1" as const
+export const AUTH_REFRESH = "/user/refresh" as const
 export const API_SIGNIN = "/user/sign-in" as const
+export const API_SIGNOUT = "/user/sign-out" as const
