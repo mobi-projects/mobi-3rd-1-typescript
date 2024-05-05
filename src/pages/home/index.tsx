a177110d568dc1f46c09756d08da08dd7b27d129
snkchan <144839872+snkchan@users.noreply.github.com>
2024-05-05 22:07:30 +0900

    feat : 임시 header에 임시 my-page버튼 추가

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,8 +3,11 @@ import { usePagination } from "@/components/pagination/pagination.hook"
 import { postUserSignOut } from "../sign-in/sign-in.func"
 import { BOOK_LIST_TOT } from "./home.constant"
 import { useBookList } from "./home.hook"
+import { useNavigate } from "react-router-dom"
+import { PATH_MY } from "@/constants"
 
 export const Home = () => {
+  const navi = useNavigate()
   const { page } = usePagination({ totalPageLength: BOOK_LIST_TOT })
   const { data } = useBookList({ page: page })
   console.log(page)
@@ -12,14 +15,22 @@ export const Home = () => {
   return (
     <div>
       <div className="flex h-[8rem] w-[40rem] items-center justify-between bg-green-300">
-      <h1>임시 헤더입니다. 로그아웃기능을 위해 배치했습니다.</h1>
-      <button
-        onClick={async () => await postUserSignOut()}
-        className="rounded-3xl border-2 border-black bg-blue-600 p-3"
-      >
-        Logout
-      </button>
-    </div>
+        <h1>임시 헤더입니다. 로그아웃기능을 위해 배치했습니다.</h1>
+        <button
+          onClick={async () => await postUserSignOut()}
+          className="rounded-3xl border-2 border-black bg-blue-600 p-3"
+        >
+          Logout
+        </button>
+        <button
+          onClick={() => {
+            navi(PATH_MY)
+          }}
+          className="rounded-3xl border-2 border-black bg-blue-600 p-3"
+        >
+          go mypage
+        </button>
+      </div>
       <ul>
         {data?.item.map((book) => (
           <li key={book.isbn13}>
