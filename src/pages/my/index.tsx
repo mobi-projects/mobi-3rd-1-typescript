59d778af31174486e68196ff617e8e85640d0a1d
snkchan <144839872+snkchan@users.noreply.github.com>
2024-05-06 01:02:43 +0900

    feat : my페이지 유저정보 조회&변경 기능구현

--- src/pages/my/index.tsx
+++ src/pages/my/index.tsx
@@ -1,66 +1,47 @@
-import { API_UPDATE_USER_INFO } from "@/constants/server-endpoint"
-import { baseAxiosInstance } from "@/libs/axios"
-import { useQuery } from "@tanstack/react-query"
-import { postUserSignOut } from "../sign-in/sign-in.func"
-import { useNavigate } from "react-router-dom"
-import { PATH_MY } from "@/constants"
 import { DeferableImg } from "@/components"
+import { useDialog } from "@/components/dialog/dialog.hook"
+import { Header } from "@/components/header"
+import { useFetchingUserInfo } from "./my.hook"
+import { UpadateUserInfoModal } from "./my.modal"
 
+/**
+ * @notice
+ * - 유저정보의 데이터가없다면? 기본값을 보여줘라 라는 코드를 작성해두었는데 임시로 작성해둔것입니다. 수정해야함
+ */
 export const My = () => {
-  const navi = useNavigate()
-  type UserDataType = {
-    data: Record<"nickName" | "location", string>
-    profileUrl: string | null
-    userId: string
+  const { onModal } = useDialog()
+  const { data, isLoading } = useFetchingUserInfo()
+  const onClickConfirm = () => {
+    // 확인누르면 실행할 로직작성
+    console.log("실행")
   }
-  type PatchUserInfoFT = () => Promise<UserDataType>
-
-  const patchUserInfo: PatchUserInfoFT = async () => {
-    try {
-      const response = await baseAxiosInstance.patch(API_UPDATE_USER_INFO)
-      return response.data
-    } catch (err) {
-      console.log(err)
-    }
-  }
-  const { data, isLoading } = useQuery<UserDataType>({
-    queryKey: ["userInfo"],
-    queryFn: () => patchUserInfo(),
-  })
   if (isLoading) return <h1>Loading중....</h1>
-  console.log(data)
-
   return (
     <div className=" h-full  w-full ">
-      <div className="flex h-[8rem] w-[40rem] items-center justify-between bg-green-300">
-        <h1>임시 헤더입니다. 로그아웃기능을 위해 배치했습니다.</h1>
-        <button
-          onClick={async () => await postUserSignOut()}
-          className="rounded-3xl border-2 border-black bg-blue-600 p-3"
-        >
-          Logout
-        </button>
-        <button
-          onClick={() => {
-            navi(PATH_MY)
-          }}
-          className="rounded-3xl border-2 border-black bg-blue-600 p-3"
-        >
-          go mypage
-        </button>
-      </div>
+      <Header />
       <div className="h-full w-[40rem]">
         <DeferableImg
           src={data?.profileUrl || "https://source.unsplash.com/random/?man"}
         />
-        <span className="bg-green-300">
+        <p className="h-[3rem] w-fit bg-green-300">
           {data?.data.nickName || "닉네임입력좀"}
-        </span>
-        <span className="bg-blue-300">
+        </p>
+        <p className="h-[3rem]  bg-blue-300">
           {data?.data.location || "주소입력좀"}
-        </span>
-        <span className="bg-slate-300">{data?.userId || "loginId"}</span>
+        </p>
+        <p className="h-[3rem]  bg-slate-300">{data?.userId || "loginId"}</p>
       </div>
+      <button
+        onClick={() =>
+          onModal({
+            onConfirm: onClickConfirm,
+            children: <UpadateUserInfoModal />,
+          })
+        }
+        className="h-fit w-fit rounded-3xl bg-slate-200 p-4 hover:bg-slate-400"
+      >
+        정보 수정하기
+      </button>
     </div>
   )
 }
