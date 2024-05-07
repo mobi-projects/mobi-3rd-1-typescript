e7db34660f488a0b91e4b87112979018f7182013
snkchan <144839872+snkchan@users.noreply.github.com>
2024-05-08 04:31:48 +0900

    feat : sign-header 생성

---
+++ src/pages/sign/sign-up/sign-up.header.tsx
@@ -0,0 +1,10 @@
+export const SignUpHeader = () => {
+  return (
+    <div className="flex h-[13rem] w-full flex-col gap-3">
+      <h1 className="text-4xl font-extrabold">Chaeg Check</h1>
+      <h2 className=" text-xl font-semibold">
+        Let's sign up and share our book reviews together
+      </h2>
+    </div>
+  )
+}
