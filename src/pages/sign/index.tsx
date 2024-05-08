581c8b940f5bf28e9440ddd4dea21cd382af1acb
snkchan <144839872+snkchan@users.noreply.github.com>
2024-05-08 11:07:32 +0900

    chore : 불필요한 태그 제거

--- src/pages/sign/index.tsx
+++ src/pages/sign/index.tsx
@@ -7,29 +7,27 @@ import { SignUpHeader } from "./sign-up/sign-up.header"
 
 export const Sign = () => {
   return (
-    <div className="IPHON_XR:px-2">
-      <Tabs defaultValue={D_LOGIN} className="w-full pt-24">
-        <TabsContent value={D_LOGIN}>
-          <SignInHeader />
-        </TabsContent>
-        <TabsContent value={D_SIGN_UP}>
-          <SignUpHeader />
-        </TabsContent>
-        <TabsList className="dark w-full">
-          <TabsTrigger value={D_LOGIN} className="w-full">
-            Login
-          </TabsTrigger>
-          <TabsTrigger value={D_SIGN_UP} className="w-full ">
-            Sign UP
-          </TabsTrigger>
-        </TabsList>
-        <TabsContent value={D_LOGIN}>
-          <SignInForm />
-        </TabsContent>
-        <TabsContent value={D_SIGN_UP}>
-          <SignUpForm />
-        </TabsContent>
-      </Tabs>
-    </div>
+    <Tabs defaultValue={D_LOGIN} className="IPHON_XR:px-2 w-full pt-24">
+      <TabsContent value={D_LOGIN}>
+        <SignInHeader />
+      </TabsContent>
+      <TabsContent value={D_SIGN_UP}>
+        <SignUpHeader />
+      </TabsContent>
+      <TabsList className="dark w-full">
+        <TabsTrigger value={D_LOGIN} className="w-full">
+          Login
+        </TabsTrigger>
+        <TabsTrigger value={D_SIGN_UP} className="w-full ">
+          Sign UP
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent value={D_LOGIN}>
+        <SignInForm />
+      </TabsContent>
+      <TabsContent value={D_SIGN_UP}>
+        <SignUpForm />
+      </TabsContent>
+    </Tabs>
   )
 }
