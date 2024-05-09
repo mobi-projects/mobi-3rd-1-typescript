40d486da5a9ace2006a4ee75fba31483ad9b2140
yunshin <2younsin@gmail.com>
2024-05-09 14:51:30 +0900

    feat : type-guard 함수 작성

--- src/funcs/index.ts
+++ src/funcs/index.ts
@@ -1,3 +1,5 @@
+export * from "./manage-storage"
+export * from "./type-guard"
 /** [공용 일반함수 관리 폴더]
 ---------------------------------------------------------------------------
 ## description
---
+++ src/funcs/type-guard/index.ts
@@ -0,0 +1,8 @@
+export const isUndefined = (input: unknown): input is undefined =>
+  typeof input === "undefined"
+
+export const isNull = (input: unknown): input is null =>
+  typeof input === "object" && !!!input
+
+export const isString = (input: unknown): input is string =>
+  typeof input === "string"
