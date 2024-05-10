e2db8868be49660990c8e7ab27cc49c9fcb40714
snkchan <144839872+snkchan@users.noreply.github.com>
2024-05-11 05:16:45 +0900

    refactor : detail페이지 반응형 추가

--- src/pages/detail/components/detail.title.tsx
+++ src/pages/detail/components/detail.title.tsx
@@ -2,9 +2,9 @@ import type { BookDetailType } from "../detail.type"
 
 export const DetailTitle = ({ title, subInfo }: BookDetailType) => {
   return (
-    <div className="flex h-fit w-full items-center overflow-hidden border-b-2 py-5 IPHON_XR:py-2 ">
+    <div className="flex flex-col h-fit w-full items-start  border-b-2 py-5 IPHON_XR:py-2 ">
       <h2 className="flex  text-xl font-bold">{title}</h2>
-      <p className="flex items-center pl-2 pt-1 text-slate-500 IPHON_XR:hidden">
+      <p className="flex items-center pl-2 pt-1 text-slate-500 IPHON_XR:hidden SF_DUO:hidden">
         {subInfo.subTitle}
       </p>
     </div>
--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -11,9 +11,9 @@ export const Detail = () => {
   if (!isBookDetail(data)) return <div>데이터가 없습니다.</div>
 
   return (
-    <div className="flex w-full items-center justify-center px-2 ">
-      <div className=" flex w-[70rem] items-center justify-center IPHON_XR:w-full">
-        <div className="flex h-fit w-full flex-col items-start">
+    <div className="flex w-full items-center justify-center px-2">
+      <div className=" flex  w-full items-center justify-center px-2 IPAD_PRO:w-[50rem]">
+        <div className="flex h-fit w-full flex-col items-start ">
           <DetailTitle {...data} />
           <DetailContent {...data} />
           <Review bookId={bookId} {...data} />
