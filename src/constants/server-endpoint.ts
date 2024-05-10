71cfc8c91542eb25d195d42cf9718b79269c9a0f
yunshin <2younsin@gmail.com>
2024-05-10 13:00:22 +0900

    chore : 유저 정보를 획득할 수 있는 endpoint 등록

--- src/constants/server-endpoint.ts
+++ src/constants/server-endpoint.ts
@@ -4,3 +4,4 @@ export const API_UPDATE_USER_INFO = "/user/update/info" as const
 export const API_SIGN_UP = "/user/sign-up" as const
 export const API_UPDATE_USER_IMAGE = "/user/update/profileUrl" as const
 export const API_BOOK = "/data/book" as const
+export const API_GET_USER = "/user/update/info"
