fbb6b5703ac42294323ff73e8049f9a8a3809bc8
yunshin <2younsin@gmail.com>
2024-05-11 17:59:44 +0900

    feat : useFetchingUserInfo(), useUser() 로 대체

--- src/pages/my/my.func.ts
+++ src/pages/my/my.func.ts
@@ -4,16 +4,7 @@ import {
 } from "@/constants/server-endpoint"
 import { baseAxiosInstance } from "@/libs/axios"
 
-import type { PatchUserInfoFT, PatchUserUpdateInfoFT } from "./my.type"
-
-export const patchUserInfo: PatchUserInfoFT = async () => {
-  try {
-    const response = await baseAxiosInstance.patch(API_UPDATE_USER_INFO)
-    return response.data
-  } catch (err) {
-    console.log(err)
-  }
-}
+import type { PatchUserUpdateInfoFT } from "./my.type"
 
 export const patchUserUpdateInfo: PatchUserUpdateInfoFT = async (newData) => {
   try {
