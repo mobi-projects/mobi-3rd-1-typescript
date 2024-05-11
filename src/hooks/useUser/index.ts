d249bfd027e84c33023d2a3882ad8cb6fea6852c
yunshin <2younsin@gmail.com>
2024-05-11 17:56:40 +0900

    feat : userUser, data alias

--- src/hooks/useUser/index.ts
+++ src/hooks/useUser/index.ts
@@ -2,10 +2,12 @@ import { QUERY_KEY_USER } from "@/constants"
 import { useQuery } from "@tanstack/react-query"
 import { getUserFromPeanut } from "./useUser.func"
 
-export const useUser = () =>
-  useQuery({
+export const useUser = () => {
+  const { data: user, ...rest } = useQuery({
     queryKey: [QUERY_KEY_USER],
     queryFn: getUserFromPeanut,
     staleTime: 1000 * 60 * 30,
     gcTime: 1000 * 60 * 45,
   })
+  return { user, ...rest }
+}
