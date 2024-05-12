53101db601262d471785dd376f5118d27c3283e5
yunshin <2younsin@gmail.com>
2024-05-12 18:28:27 +0900

    feat : 에러메시지 출력

--- src/pages/detail/components/detail.review.tsx
+++ src/pages/detail/components/detail.review.tsx
@@ -8,6 +8,7 @@ import { isUndefined } from "@/funcs"
 import { useUser } from "@/hooks"
 import { createReview } from "../detail.func"
 import type { BookDetailType, ReviewFormType } from "../detail.type"
+import { ErrorMsg } from "./error-msg"
 
 export const Review = ({
   bookId,
@@ -24,24 +25,30 @@ export const Review = ({
 
   return (
     <form className="pt-5" onSubmit={handleSubmit(onSubmitReview)}>
-      <h1 className="py-2 text-lg font-bold">후기 남기기</h1>
+      <div className="flex h-fit w-full items-end justify-start gap-9 ">
+        <h1 className="w-fit whitespace-nowrap py-2 text-lg font-bold">
+          댓글 남기기
+        </h1>
+        <ErrorMsg errorField={errors[FORM_COMMENT]} />
+      </div>
       <div className="w-full">
         <Textarea
           className="resize-none  border-2 border-slate-500 IPHON_XR:w-[21rem] SF_DUO:w-[25rem] IPAD_PRO:w-[45rem] "
           {...register(FORM_COMMENT)}
-          placeholder="후기를 남겨주세요!"
+          placeholder="댓글를 남겨주세요!"
         />
       </div>
-      <div className="flex items-center py-3">
-        <p className="pr-3">별점등록 : </p>
+      <div className="flex items-center gap-4 py-3">
+        <p className="whitespace-nowrap  pr-3">별점등록 : </p>
         <Input
           {...register(FORM_RATING)}
-          className="flex h-8 w-[7rem] border-2 border-slate-500 pl-3 IPHON_XR:w-[5.5rem]"
+          className="flex h-8 w-[10rem] border-2 border-slate-500 pl-3 IPHON_XR:w-[5.5rem]"
           type="number"
           placeholder="별점등록"
           max={10}
           min={0}
         />
+        <ErrorMsg errorField={errors[FORM_RATING]} />
       </div>
       <Button className="border-2 border-solid" disabled={!isValid}>
         리뷰등록
