dc34a5781d19ef89ab5cc2086272e9777de3dafa
yunshin <2younsin@gmail.com>
2024-05-12 21:19:05 +0900

    depend : react-error-boundary

--- src/libs/react-query/default-query-client.ts
+++ src/libs/react-query/default-query-client.ts
@@ -1,3 +1,10 @@
 import { QueryClient } from "@tanstack/react-query"
 
-export const defaultQueryClient = new QueryClient({})
+export const defaultQueryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+      throwOnError: true,
+    },
+  },
+})
