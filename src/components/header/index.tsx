67b9895ef829ae3216e8275469b4fd5dfeeb2d3e
snkchan <144839872+snkchan@users.noreply.github.com>
2024-05-15 14:42:19 +0900

    feat : header에 logo추가

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,14 +4,20 @@ import { useNavigate } from "react-router-dom"
 import { PATH_HOME, PATH_MY } from "@/constants"
 import { Button } from "../ui/button"
 import type { ButtonIconPT } from "./header.type"
-
+import logo from "/chaeg-check.svg"
 export const Header = () => {
   const navigate = useNavigate()
   const { logout } = useSignOut()
 
   return (
-    <div className=" flex h-[6rem] w-full flex-col justify-center border-b-2">
-      <div className="flex h-1/2 w-full items-center justify-center font-extrabold">
+    <div className=" relative flex h-[6rem] w-full flex-col justify-center border-b-2 bg-slate-50">
+      <img
+        src={logo}
+        alt="Logo"
+        className="absolute left-5 h-full w-16 cursor-pointer rounded-full IPHON_XR:left-1 IPHON_XR:top-1 IPHON_XR:h-1/2"
+        onClick={() => navigate(PATH_HOME)}
+      />
+      <div className="flex h-1/2 w-full items-center justify-center font-extrabold IPAD_PRO:text-2xl">
         Chaeg Check
       </div>
       <div className="flex h-1/2 w-full justify-end IPHON_XR:justify-evenly ">
