4e2be65cfc06a5d3d3b9460f3a3df78cf0d44906
keeprok <140785214+keeprok@users.noreply.github.com>
2024-05-16 14:52:24 +0900

    publish : 반응형 디자인 수정

--- src/pages/my/components/my.profile-image.tsx
+++ src/pages/my/components/my.profile-image.tsx
@@ -15,7 +15,7 @@ export const ProfileImage = ({ profileUrl }: UserType) => {
   return (
     <div className=" flex items-center justify-center IPHON_XR:h-[15rem] SF_DUO:h-[50%] IPAD_PRO:h-[29rem] IPAD_PRO:justify-start">
       <div className=" flex h-full w-[18rem]  flex-col items-center  justify-center rounded-xl border-2 border-solid border-slate-300 bg-slate-200 drop-shadow-2xl">
-        <div className=" flex h-full w-full flex-col  items-center justify-between pb-[2rem] pt-[2rem]">
+        <div className=" flex h-full w-full  flex-col items-center justify-between pb-[2rem] pt-[2rem] IPHON_XR:pb-4 IPHON_XR:pt-4">
           <div className="relative h-[13rem]  w-[13rem] cursor-pointer   rounded-3xl">
             <img
               src={
@@ -36,7 +36,7 @@ export const ProfileImage = ({ profileUrl }: UserType) => {
           </div>
           <img
             src={MainLogo}
-            className=" h-[4.5rem] w-[4.5rem] rounded-full IPHON_XR:h-[3rem] IPHON_XR:w-[3rem]"
+            className=" h-[4.5rem] w-[4.5rem] rounded-full IPHON_XR:hidden "
           />
         </div>
       </div>
